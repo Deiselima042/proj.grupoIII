749dfd5381433e5fd99bccd3e9bcd995b02306b9
Deiselima042 <113534930+Deiselima042@users.noreply.github.com>
2022-12-14 12:03:16 -0300

    atualizaçao

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import Montanha_1 from './imagens/imagem_1.jpg';
-import Montanha_2 from './imagens/imagem_2.jpg';
-import Montanha_3 from './imagens/imagem_3.jpg';
+import Montanha_1 from './imagens/imagem_1.png';
+import Montanha_2 from './imagens/imagem_2.png';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,8 +11,9 @@ function App() {
     <div className='App'>
 
 <header> 
-  <button>BUTAO_1</button>
-  <button>BUTAO_2</button>
+  <button>HOME</button>
+  <button>FILME</button>
+  <button>CINEMA</button>
 </header>
 
 <t>Título da imagem 1</t>
@@ -27,13 +28,6 @@ function App() {
 <br></br>
 <img src={Montanha_2}></img>
 
-
-<br></br>
-
-<t>Título da imagem 3</t>
-
-<br></br>
-<img src={Montanha_3}></img>
 
 
 <footer>
