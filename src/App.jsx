c7c6bbdc1daa5aa4d0acade8977043c9822f6f5b
FellyBrasil <113535234+FellyBrasil@users.noreply.github.com>
2022-12-16 12:05:37 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Cards from './componentes/cinemas/Cards'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Cards/>
+    </div>
+  )
+}
+
+export default App
