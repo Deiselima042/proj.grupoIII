2b218c139e9dcaab9cb005b2a9fa500e0e51f07a
Gabriel Paiva <51544207+gabrielp20@users.noreply.github.com>
2022-12-19 22:37:37 -0300

    Add files via upload

---
+++ src/AppCinemas.jsx
@@ -0,0 +1,71 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+// ########### SEU CSS ########### > import "./style_cinemas.css";
+
+
+import React from "react";
+import './Cards.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+function AppCinemas() {
+  const [info, setInfo] = useState([])
+
+  useEffect(() => {
+
+    axios.get('https://projeto-servidor.onrender.com/cinemas')
+      .then((res) => setInfo(res.data))
+
+  }, [])
+
+
+
+  return (
+
+    <div className="box">
+
+
+
+        <div className="card-header">
+
+          {info.length === 0 ? (<img src="https://thumbs.gfycat.com/MeekBoringGnat-size_restricted.gif" />) : (
+            info.map((cinema) => (
+              <div>
+                <div className="cinema" key={cinema.id}>
+                  <ul>
+                    <li><img className="img-cards-cinema" src={cinema.imagem} alt="" /></li>
+                    <li><span><h3>Cinema:</h3></span> {cinema.cine_nome}</li>
+                    <li><span><h3>Localização:</h3></span> {cinema.localizacao}</li>
+                    <li><span><h3>Horario:</h3></span> {cinema.horario_dos_filmes}</li>
+                    <li><span><h3>Assentos:</h3></span>{cinema.assentos}</li>
+                    <li><span><button>Comprar Ingresso:</button></span></li>
+
+                  </ul>
+                  <br />
+                  <hr className='hr' />
+                  <br />
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+
+  
+
+    </div>
+
+
+
+  )
+
+}
+
+export default AppCinemas
+
+
+
+
+
+
+
+
---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import App from './App'
+
+import Catalogo from "./routes/Catalogo";
+import Filme from "./routes/Filme";
+import NovoFilme from "./routes/NovoFilme";
+import EditarFilme from "./routes/EditarFilme";
+import Resultadobusca from "./routes/Resultadobusca"
+
+import Home from "./AppHome"
+import Cinemas from "./AppCinemas"
+
+
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      
+      {
+        path: "/todos_os_filmes",
+        element: <Catalogo />,
+      },
+      {
+        path: "/todos_os_filmes/:id",
+        element: <Filme />,
+      },
+      {
+        path: "/adicionar_filme",
+        element: <NovoFilme />,
+      },
+      {
+        path: "/editar_filme/:id",
+        element: <EditarFilme />,
+      },
+      {
+        path: "/busca/:busca",
+        element: <Resultadobusca />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/cinemas",
+        element: <Cinemas />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
